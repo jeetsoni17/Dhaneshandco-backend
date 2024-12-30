9b7dbf26d2445b9b16441b74c8dfcf8184bc94e2
Jeet Soni <jeetsoni1702@gmail.com>
2024-12-30 15:46:39 +0530

    back to og

--- routes/index.php
+++ routes/index.php
@@ -8,21 +8,15 @@ $request = $_GET['endpoint'] ?? null;
 // Include the necessary API file based on the endpoint
 switch ($request) {
     case 'products':
-        // If the endpoint is 'products', fetch the product by its ID
-        if (isset($_GET['id'])) {
-            $product_id = $_GET['id'];
-            include_once '../api/products.php'; // Include the product fetch script
-            getProductById($product_id); // Call function to fetch product details
-        } else {
-            http_response_code(400);
-            echo json_encode(["error" => "Product ID is required"]);
-        }
+        include_once '../api/products.php';
         break;
     case 'categories':
         include_once '../api/categories.php';
         break;
     case 'pricelist':
         include_once '../api/pricelist.php';
+        case 'subcategories':
+            include_once '../api/subcategories.php';
         break;
     default:
         http_response_code(404);
