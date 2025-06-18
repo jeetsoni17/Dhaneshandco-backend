0d5b261253f43a5eceeea0e8c54c6b4c598c60f4
Jeet Soni <jeetsoni1702@gmail.com>
2025-06-18 16:34:15 +0530

    undo

--- api/products.php
+++ api/products.php
@@ -1,8 +1,5 @@
 <?php
 // Show PHP errors for debugging (remove in production)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
 include_once './routes/index.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// Define the base URL for the application
